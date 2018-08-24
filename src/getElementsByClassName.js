4a4d4f130041a17b3545c855ddaa02238aa9306b
Yuki <yukiej@gmail.com>
2018-08-23 21:40:52 -0700

    Pass all tests using document.body, element.childNodes, end element.classList

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+	return getElementsByClassNameHelper(className,document.body);
+}
+
+var getElementsByClassNameHelper = function(className, node) {
+	var results = [];
+
+  //use document.body to get the <body> element of the page
+  //use element.childNodes to get list of the child nodes of body 
+  //use element.classList to get the DOMTokenList collection of the class attributes of the element, which should be added to results.
+  //do steps 2 and 3 recursively until there are no child nodes
+  
+  	var childNodes = node.childNodes;
+  	if (node.classList !== undefined && node.classList.contains(className)){
+  		results.push(node);	
+  	}
+  	if (childNodes !== undefined){
+	  	for (var index = 0; index < childNodes.length; index++){
+	  		var $child = childNodes[index];
+	  		results = results.concat(getElementsByClassNameHelper(className,$child));
+	  	}
+	}
+  	return results;
+}
+  
